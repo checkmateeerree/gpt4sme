4669416a204eac96b22a5f7ffdb5d057a6882e80
Alan Hu <chessplayer292@gmail.com>
2023-08-09 23:25:44 -0400

    added title to home page

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,16 @@ import WorkingWithBusinesses from "../components/landing_page/workwithbusinesses
 import CallToAction from "../components/landing_page/calltoaction";
 import {Flex, Heading, Text, VStack, HStack, Box, Wrap} from "@chakra-ui/layout"
 import {Button, useToast} from "@chakra-ui/react"
+import Head from "next/head"
 
 function Index() {
   
   return (
     <>
+      <Head>
+        <title>GPT4SME: An organization that seeks to demystify the powers of AI to smaller and medium sized enterprises</title>
+        <meta property="og:title" content="GPT4SME: An organization that seeks to demystify the powers of AI to smaller and medium sized enterprises" key="title" />
+      </Head>
       {true && (
           <>
             <Hero />
