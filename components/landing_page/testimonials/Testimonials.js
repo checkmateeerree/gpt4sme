f3428debfae00e8a00069da26210fd85def98cce
Alan Hu <chessplayer292@gmail.com>
2023-10-21 19:06:07 -0400

    deleted garbage imports

--- components/landing_page/testimonials/Testimonials.js
+++ components/landing_page/testimonials/Testimonials.js
@@ -1,6 +1,4 @@
 import React from 'react'
-import {Carousel} from 'react-responsive-carousel'
-import "react-responsive-carousel/lib/styles/carousel.min.css"; 
 import { Stack, Box, Heading, Center, Grid, GridItem, SimpleGrid } from '@chakra-ui/react';
 import Company from './Company.js';
 import CompanyList from "../../../utils/LandingPageCompanyList.js";
